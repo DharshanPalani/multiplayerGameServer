521ab222ce37ef2d5274a604f314ce872c33cecf
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-14 21:41:57 +0530

    Added selecting random player as a artist

--- modules/startGame.ts
+++ modules/startGame.ts
@@ -4,15 +4,29 @@ const startGame = (clientConnection) => {
   const currentRoom = clientConnection.currentRoom;
   if (!currentRoom || !roomMap.has(currentRoom)) return;
 
+  var clientArray: any = [];
+
   const connectedClientsInRoom = roomMap.get(currentRoom);
   for (const roomClient of connectedClientsInRoom) {
     if (roomClient.readyState === WebSocket.OPEN) {
+      clientArray.push(roomClient);
       const startGameMessage = {
         type: "start_game",
       };
       roomClient.send(JSON.stringify(startGameMessage));
     }
   }
+
+  if (clientArray.length === 0) return;
+
+  const randomIndex = Math.floor(Math.random() * clientArray.length);
+  const chosenClient = clientArray[randomIndex];
+
+  const assignArtistMessage = {
+    type: "assign_artist",
+  };
+
+  chosenClient.send(JSON.stringify(assignArtistMessage));
 };
 
 export default startGame;
